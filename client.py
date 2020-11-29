c99189779ef6cfa889899bdabc2f41dcc8bbe223
Niraj Patel <786nirajpatel@gmail.com>
2020-11-29 02:23:10 -0500

    3rd One-to-One job is complete and works perfectly

--- client.py
+++ client.py
@@ -1,8 +1,9 @@
 import socket
 import sys
+from scapy.all import ARP, Ether, srp
 
 HOST = '127.0.0.1'  # The server's hostname or IP address
-PORT = 65432        # The port used by the server
+PORT = 65432       # The port used by the server
 CLIENT_NUM = None
 JOB = None
 
@@ -32,14 +33,28 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client:
             print("Client: Client #" + str(CLIENT_NUM) + " the current job is: " + JOB)
             continue
             
-        elif command == "SEND JOB":
-            if JOB != None:
-                print("Client: Client #" + str(CLIENT_NUM) + " the current job is: " + JOB)
-                answer = input("Client: Please enter your answer: ")
+        elif command == "SEND JOB" and JOB:
+            print("Client: Client #" + str(CLIENT_NUM) + " the current job is: ",
+                  "Detect all live IP addresses on a given subnet: ", JOB[2:]+"/24")
+            if JOB[0] == "3":
+                target_ip = JOB[2:] + "/24"
+                arp = ARP(pdst=target_ip)
+                ether = Ether(dst="ff:ff:ff:ff:ff:ff")
+                packet = ether/arp
+                result = srp(packet, timeout=5, verbose=1)[0]
+                ips = []
+                for sent, received in result:
+                    ips.append({'ip': received.psrc, 'mac':received.hwsrc})
+                print("Abaliable devices in the network: ")
+                print("IP" + " "*18 + "MAC")
+                for ip in ips:
+                    print("{:16}    {}".format(ip['ip'], ip['mac']))    
+               
                 client.sendall(str.encode(command))
-                client.sendall(str.encode(answer))
+                client.sendall(str(ips).encode())
             else:
                 print("NO JOB TAKEN")
+            continue
         
         elif command == "\help":
             print("""Commands Avaliable: 
--- server.py
+++ server.py
@@ -7,10 +7,11 @@ HOST = '127.0.0.1'
 PORT = 65432
 THREADCOUNT = 0
 CLIENT_WITH_JOB = set()
-JOBS = ['3+3', '5*6+9']
+JOBS = ['3 ' + socket.gethostbyname(socket.gethostname())]
 
 def threaded_server(connection, addr):
-    print(addr)
+    global THREADCOUNT
+    print(addr, THREADCOUNT)
     job_given = False
     current_job = None
     connection.send(str.encode("220 " + str(THREADCOUNT)))
@@ -25,32 +26,32 @@ def threaded_server(connection, addr):
         
         elif data.decode('utf-8') == "QUIT":
             print("Server: 221 Client #" + str(THREADCOUNT) + " with address of " + str(addr[0]), str(addr[1]) + " closing connection")
+            THREADCOUNT -= 1
             break
         
         elif data.decode('utf-8') == "REQUEST JOB" and not job_given:
-            current_job = random.choice(JOBS)
-            connection.sendall(str.encode("250 " + str(current_job)))
-            job_given = True
-            print("Server: 250 job send to Client #" + str(THREADCOUNT) + " with address of " + str(addr[0]), str(addr[1]))
+            if THREADCOUNT == 1:
+                current_job = random.choice(JOBS)
+                connection.sendall(str.encode("250 " + str(current_job)))
+                job_given = True
+                print("Server: 250 job send to Client #" + str(THREADCOUNT) + " with address of " + str(addr[0]), str(addr[1]))
         
         elif data.decode('utf-8') == "REQUEST JOB" and job_given:
             connection.sendall(str.encode("452"))
             print("Server: 452 job is already sent to Client #" + str(THREADCOUNT) + " with address of " + str(addr[0]), str(addr[1]))
         
         elif data.decode('utf-8') == "SEND JOB":
-            answer = connection.recv(2048)
+            answer = connection.recv(4096)
             job_given = False
-            if int(eval(current_job)) == int(answer.decode('utf-8')):
-                connection.sendall(str.encode("251"))
-                print("Server: 251 Client #" + str(THREADCOUNT) + " with address of " + str(addr[0]), str(addr[1]) + " got the right answer")
-            else:
-                connection.sendall(str.encode("453"))
-                print("Server: 251 Client #" + str(THREADCOUNT) + " with address of " + str(addr[0]), str(addr[1]) + " got the wrong answer")
+            data = answer.decode('utf-8')
+            data = eval(data)
+            print("Abaliable devices in the network: ")
+            print("IP" + " "*18 + "MAC")
+            for ip in data:
+                print("{:16}    {}".format(ip['ip'], ip['mac'])) 
+            print("Server: 251 Client #" + str(THREADCOUNT) + " with address of " + str(addr[0]), str(addr[1]) + " did the job correctly")
             current_job = None
-            
-    connection.close()
-        
-        
+    connection.close() 
 
 def main():
     global THREADCOUNT
